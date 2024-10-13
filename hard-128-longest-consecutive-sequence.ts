000144f79d02a0e360c91c73b1243bb766a15b76
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-13 12:53:43 +0300

    Problem 128 longest-consecutive-sequence

---
+++ hard-128-longest-consecutive-sequence.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/longest-consecutive-sequence/
+
+function longestConsecutive(nums: number[]): number {
+    const set = new Set(nums);
+    let output = 0;
+    for (const item of set.values()) {
+        if (!set.has(item - 1)) { // This is the trick, without it => O(n^2)
+            let count = 1;
+            let nextItem = item;
+            while (set.has(++nextItem)) {
+                count++;
+            }
+            output = Math.max(output, count);
+        }
+    }
+    return output;
+};

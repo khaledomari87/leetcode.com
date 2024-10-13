820cfd2c9d7cc21395a9c331035cbc9cc778e094
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-13 14:46:12 +0300

    Problem 344 reverse-string

---
+++ easy-344-reverse-string.ts
@@ -0,0 +1,11 @@
+// https://leetcode.com/problems/reverse-string/submissions/1420930911/
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+function reverseString(s: string[]): void {
+    for (let left = 0; left < s.length / 2; left++) {
+        const right = s.length - left - 1;
+        [s[left], s[right]] = [s[right], s[left]];
+    }
+};

3094ed9ddbe1078e8205d685501b944a7bea2853
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-13 16:33:48 +0300

    Create easy-125-valid-palindrome.ts

---
+++ easy-125-valid-palindrome.ts
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/valid-palindrome/
+
+function isPalindrome(s: string): boolean {
+    const isToSkip = (char: string): boolean => (char < 'A' || char > 'Z') && (char < '0' || char > '9');
+    let left = 0, right = s.length - 1;
+    while(left < right) {
+        const lChar = s[left].toUpperCase();
+        if (isToSkip(lChar)) {
+            left++;
+            continue;
+        }
+        const rChar = s[right].toUpperCase();
+        if (isToSkip(rChar)) {
+            right--;
+            continue;
+        }
+        if (lChar !== rChar) {
+            return false;
+        }
+        right--;
+        left++;
+    }
+    return true;
+};

0eb26414cfc0e7cca72d0004e54cdab6febe6e6b
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-14 22:36:46 +0300

    Create medium.155.min-stack.ts

---
+++ medium.155.min-stack.ts
@@ -0,0 +1,21 @@
+class MinStack {
+    constructor(private stack: number[], private minStack: number[]) {
+        this.stack = [];
+        this.minStack = [];
+    }
+    push(val: number): void {
+        this.stack.push(val);
+        const lastIndex = this.minStack.length - 1;
+        this.minStack.push(lastIndex < 0 ? val : Math.min(val, this.minStack[lastIndex]));
+    }
+    pop(): void {
+        this.stack.pop();
+        this.minStack.pop();
+    }
+    top(): number {
+        return this.stack[this.stack.length - 1];
+    }
+    getMin(): number {
+        return this.minStack[this.minStack.length - 1];
+    }
+}

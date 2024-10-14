97479359308f5e5d7743143fc5bbd1737fa56d96
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-14 14:32:51 +0300

    Create medium-11-container-with-most-water.ts

---
+++ medium-11-container-with-most-water.ts
@@ -0,0 +1,13 @@
+// https://leetcode.com/problems/container-with-most-water/
+
+function maxArea(height: number[]): number {
+    let left = 0,
+        right = height.length - 1,
+        output = 0
+    ;
+    while (left < right) {
+        output = Math.max(output, Math.min(height[left], height[right]) * (right - left));
+        height[left] > height[right] ? right-- : left++;
+    }
+    return output;
+}

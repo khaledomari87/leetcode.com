b7798d81684231dca20f7004fcc6fb7661a0f67c
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-15 15:20:05 +0300

    Create easy.141.linked-list-cycle.ts

---
+++ easy.141.linked-list-cycle.ts
@@ -0,0 +1,21 @@
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function hasCycle(head: ListNode | null): boolean {
+    let fast = head;
+    let slow = head!;
+    while (fast && fast.next) {
+        slow = slow.next!;
+        fast = fast.next.next;
+        if (fast === slow) {
+            return true;
+        }
+    }
+    return false;
+};

72d4663487262fe06c97b147dd6ea59b9da4d28d
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-15 10:29:32 +0300

    Update medium.739.daily-temperatures.ts

--- medium.739.daily-temperatures.ts
+++ medium.739.daily-temperatures.ts
@@ -4,8 +4,7 @@ function dailyTemperatures(temperatures: number[]): number[] {
     const history = new Array<number>();
     const output = new Array<number>(temperatures.length).fill(0);
     for (let index = 0; index < temperatures.length; index++) {
-        const value = temperatures[index];
-        while (history.length > 0 && temperatures[history[history.length - 1]] < value) {
+        while (history.length > 0 && temperatures[history[history.length - 1]] < temperatures[index]) {
             const previous = history.pop()!;
             output[previous] = index - previous;
         }

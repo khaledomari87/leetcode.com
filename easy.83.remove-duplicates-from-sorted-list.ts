9d20665ce56cf7d28e6ccbdd3ad409e460a83686
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-15 12:38:25 +0300

    Create easy.83.remove-duplicates-from-sorted-list.ts

---
+++ easy.83.remove-duplicates-from-sorted-list.ts
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+class ListNode {
+    val: number
+    next: ListNode | null
+    constructor(val?: number, next?: ListNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+    }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+    let curr = head;
+    while (curr) {
+        if (curr.val === curr.next?.val) {
+            curr.next = curr.next.next;
+        } else {
+            curr = curr.next;
+        }
+    }
+    return head;
+};

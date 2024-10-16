5bea0c93d01b9790e3d555f39472ef2ae1eaad2c
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-16 17:09:59 +0300

    Create easy.367.valid-perfect-square.ts

---
+++ easy.367.valid-perfect-square.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/valid-perfect-square/
+
+function isPerfectSquare(num: number): boolean {
+    let left = 1, right = Math.ceil(num / 2);
+    while (left <= right) {
+        const mid = Math.floor((left + right) / 2);
+        const square = Math.pow(mid, 2);
+        if (square === num) return true;
+        square < num ? left = mid + 1 : right = mid - 1;
+    }
+    return false;
+};

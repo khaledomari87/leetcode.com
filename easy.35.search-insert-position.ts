baa53e2954057fb2594bc8ae0321765384ef2cf7
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-16 14:42:52 +0300

    Create easy.35.search-insert-position.ts

---
+++ easy.35.search-insert-position.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/search-insert-position/
+
+function searchInsert(nums: number[], target: number): number {
+    let L = 0, R = nums.length -1;
+    while (L <= R) {
+        const M = Math.floor((L + R ) / 2);
+        if (nums[M] === target) {
+            return M;
+        }
+        if (nums[M] < target) {
+            L = M + 1;
+        } else {
+            R = M - 1;
+        }
+    }
+    return L; // Or return R + 1
+};

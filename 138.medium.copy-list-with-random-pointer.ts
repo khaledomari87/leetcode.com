6fed7e7248f44e7694d55030426e4330852a051f
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-16 12:12:46 +0300

    Create 138.medium.copy-list-with-random-pointer.ts

---
+++ 138.medium.copy-list-with-random-pointer.ts
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/copy-list-with-random-pointer/
+
+class _Node {
+    val: number
+    next: _Node | null
+    random: _Node | null
+    constructor(val?: number, next?: _Node | null, random?: _Node | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.next = (next===undefined ? null : next)
+        this.random = (random===undefined ? null : random)
+    }
+}
+
+function copyRandomList(inputHead: _Node | null): _Node | null {
+    if (!inputHead) return inputHead;
+
+    const map = new Map([[inputHead, new _Node(inputHead.val)]]);
+
+    let clonePointer = map.get(inputHead)!;
+    let inputPointer = inputHead.next;
+    while (inputPointer) {
+        clonePointer.next = new _Node(inputPointer.val);
+        clonePointer = clonePointer.next;
+        map.set(inputPointer, clonePointer);
+        inputPointer = inputPointer.next;
+    }
+
+    clonePointer = map.get(inputHead)!;
+    inputPointer = inputHead;
+    while (inputPointer) {
+        clonePointer.random = inputPointer.random && map.get(inputPointer.random) || null;
+        inputPointer = inputPointer.next;
+        clonePointer = clonePointer.next!;
+    }
+
+    return map.get(inputHead)!;
+};

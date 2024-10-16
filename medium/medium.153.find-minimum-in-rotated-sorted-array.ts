add10278e6576ffc7bcbbf17cacf40e4bc08ecf5
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-16 20:39:34 +0300

    Create medium.153.find-minimum-in-rotated-sorted-array.ts

---
+++ medium/medium.153.find-minimum-in-rotated-sorted-array.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+
+function findMin(nums: number[]): number {
+    let L = 0, R = nums.length - 1;
+    while (L < R) {
+        const M = Math.floor((L + R) / 2);
+        nums[M] < nums[R] ? R = M : L = M + 1;
+    }
+    return nums[L];
+}

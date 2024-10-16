7d97936c332abf3b579a9f37f1951620b97795a2
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-16 15:30:56 +0300

    Create easy.278.first-bad-version.ts

---
+++ easy.278.first-bad-version.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/first-bad-version/
+
+/**
+ * The knows API is defined in the parent class Relation.
+ * isBadVersion(version: number): boolean {
+ *     ...
+ * };
+ */
+
+var solution = function (isBadVersion: (version: number) => boolean) {
+    return function (right: number): number {
+        let left = 1;
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2);
+            isBadVersion(mid) ? right = mid : left = mid + 1;
+        }
+        return left; // Or return right;
+    };
+};

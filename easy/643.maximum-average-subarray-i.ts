50dd4dd3b4623bdc1fd5b47d9bccc810850cda0b
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-17 19:31:34 +0300

    Create 643.maximum-average-subarray-i.ts

---
+++ easy/643.maximum-average-subarray-i.ts
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/maximum-average-subarray-i/
+
+function findMaxAverage(nums: number[], k: number): number {
+    let maxAvg = 0;
+    for (let i = 0; i < k; i++) {
+        maxAvg += nums[i];
+    }
+    maxAvg = maxAvg / k;
+    let L = 1;
+    let prevAvg = maxAvg;
+    while (L + k <= nums.length) {
+        prevAvg = (prevAvg * k - nums[L - 1] + nums[L + k - 1]) / k;
+        maxAvg = Math.max(
+            maxAvg,
+            prevAvg,
+        );
+        L++;
+    }
+    return maxAvg;
+}

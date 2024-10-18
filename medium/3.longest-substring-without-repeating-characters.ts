004f677a2e3f134bb23ab6ad2d58412de2e3fd27
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-18 07:21:54 +0300

    Create 3.longest-substring-without-repeating-characters.ts

---
+++ medium/3.longest-substring-without-repeating-characters.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+function lengthOfLongestSubstring(s: string): number {
+    let output = 0;
+    const set = new Set<string>();
+    for (let R = 0, L = 0; R < s.length; R++) {
+        while (set.has(s[R])) {
+            set.delete(s[L]);
+            L++;
+        }
+        set.add(s[R]);
+        output = Math.max(output, R - L + 1);
+    }
+    return output;
+}

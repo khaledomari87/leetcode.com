cc3ab99dc74c82e56824d4b71f658da40d346132
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-18 06:52:08 +0300

    Create 1004.max-consecutive-ones-iii.ts

---
+++ medium/1004.max-consecutive-ones-iii.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/max-consecutive-ones-iii/
+
+function longestOnes(nums: number[], allowance: number): number {
+    let output = 0;
+    for (let R = 0, L = 0, ZerosCount = 0; R < nums.length; R++) {
+        !nums[R] && ZerosCount++;
+        while (ZerosCount > allowance) {
+            !nums[L] && ZerosCount--;
+            L++;
+        }
+        output = Math.max(output, R - L + 1);
+    }
+    return output;
+}

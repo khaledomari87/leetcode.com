00fa25f3311a5972556d94d327d6610a38fc84c0
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-20 08:09:21 +0300

    Create 104.maximum-depth-of-binary-tree.ts

---
+++ easy/104.maximum-depth-of-binary-tree.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/maximum-depth-of-binary-tree/
+
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val===undefined ? 0 : val)
+        this.left = (left===undefined ? null : left)
+        this.right = (right===undefined ? null : right)
+    }
+}
+
+function maxDepth(root: TreeNode | null): number {
+    return root && 1 + Math.max(maxDepth(root.right), maxDepth(root.left)) || 0;
+};

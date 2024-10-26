a50e23564c40c795bd99e2b7103bffcd5ff6e66b
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-26 11:53:00 +0300

    export Heap class and update 215.kth-largest-element-in-an-array.ts

---
+++ assets/heap.ts
@@ -0,0 +1,70 @@
+export class Heap<T> {
+    private heap: T[] = [];
+    private comparator: (a: T, b: T) => number;
+    /**
+     * Construct a new Heap with the given comparator.
+     * @param comparator - The comparator to use when ordering values in the heap.
+     * For MinHeap use (a, b) => a - b, for MaxHeap use (a, b) => b - a
+     */
+    constructor(comparator: (a: T, b: T) => number) {
+        this.comparator = comparator;
+    }
+    enqueue(val: T) {
+        this.heap.push(val);
+        this.bubbleUp();
+    }
+    dequeue() {
+        const max = this.heap[0];
+        const end = this.heap.pop();
+        if (this.heap.length > 0) {
+            this.heap[0] = end!;
+            this.bubbleDown();
+        }
+        return { element: max };
+    }
+    front() {
+        return { element: this.heap[0] };
+    }
+    private bubbleUp() {
+        let idx = this.heap.length - 1;
+        const element = this.heap[idx];
+        while (idx > 0) {
+            const parentIdx = Math.floor((idx - 1) / 2);
+            const parent = this.heap[parentIdx];
+            if (this.comparator(element, parent) >= 0) break;
+            this.heap[parentIdx] = element;
+            this.heap[idx] = parent;
+            idx = parentIdx;
+        }
+    }
+    private bubbleDown() {
+        let idx = 0;
+        const length = this.heap.length;
+        const element = this.heap[0];
+        while (true) {
+            const leftChildIdx = 2 * idx + 1;
+            const rightChildIdx = 2 * idx + 2;
+            let leftChild, rightChild;
+            let swap = null;
+            if (leftChildIdx < length) {
+                leftChild = this.heap[leftChildIdx];
+                if (this.comparator(leftChild, element) < 0) {
+                    swap = leftChildIdx;
+                }
+            }
+            if (rightChildIdx < length) {
+                rightChild = this.heap[rightChildIdx];
+                if (
+                    (!swap && this.comparator(rightChild, element) < 0) ||
+                    (swap && this.comparator(rightChild, leftChild!) < 0)
+                ) {
+                    swap = rightChildIdx;
+                }
+            }
+            if (swap === null) break;
+            this.heap[idx] = this.heap[swap];
+            this.heap[swap] = element;
+            idx = swap;
+        }
+    }
+}
--- easy/215.kth-largest-element-in-an-array.ts
+++ easy/215.kth-largest-element-in-an-array.ts
@@ -1,70 +1,10 @@
 // https://leetcode.com/problems/kth-largest-element-in-an-array/
 
-class MinHeap<T> {
-    private heap: T[] = [];
-    enqueue(val: T) {
-        this.heap.push(val);
-        this.bubbleUp();
-    }
-    dequeue() {
-        const max = this.heap[0];
-        const end = this.heap.pop();
-        if (this.heap.length > 0) {
-            this.heap[0] = end!;
-            this.bubbleDown();
-        }
-        return { element: max };
-    }
-    front() {
-        return { element: this.heap[0] };
-    }
-    private bubbleUp() {
-        let idx = this.heap.length - 1;
-        const element = this.heap[idx];
-        while (idx > 0) {
-            const parentIdx = Math.floor((idx - 1) / 2);
-            const parent = this.heap[parentIdx];
-            if (element >= parent) break;
-            this.heap[parentIdx] = element;
-            this.heap[idx] = parent;
-            idx = parentIdx;
-        }
-    }
-    private bubbleDown() {
-        let idx = 0;
-        const length = this.heap.length;
-        const element = this.heap[0];
-        while (true) {
-            const leftChildIdx = 2 * idx + 1;
-            const rightChildIdx = 2 * idx + 2;
-            let leftChild, rightChild;
-            let swap = null;
-            if (leftChildIdx < length) {
-                leftChild = this.heap[leftChildIdx];
-                if (leftChild < element) {
-                    swap = leftChildIdx;
-                }
-            }
-            if (rightChildIdx < length) {
-                rightChild = this.heap[rightChildIdx];
-                if (
-                    (swap === null && rightChild < element) ||
-                    (swap !== null && rightChild < leftChild!)
-                ) {
-                    swap = rightChildIdx;
-                }
-            }
-            if (swap === null) break;
-            this.heap[idx] = this.heap[swap];
-            this.heap[swap] = element;
-            idx = swap;
-        }
-    }
-}
+import { Heap } from '../assets/heap.ts';
 
 const findKthLargest = function (nums: number[], k: number) {
     // leetcode MinPriorityQueue is slow for some reason!
-    const queue = new MinHeap<number>();
+    const queue = new Heap<number>((a, b) => a - b);
     for (let i = 0; i < nums.length; i++) {
         if (i < k) {
             queue.enqueue(nums[i]);

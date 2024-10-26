72e5c30e562a0b0c645b911b078d6c9f00c76e0e
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-26 14:33:04 +0300

    Update 215.kth-largest-element-in-an-array.ts

--- easy/215.kth-largest-element-in-an-array.ts
+++ easy/215.kth-largest-element-in-an-array.ts
@@ -2,18 +2,18 @@
 
 import { Heap } from '../assets/heap.ts';
 
-const findKthLargest = function (nums: number[], k: number) {
+const findKthLargest = function (nums: number[], k: number): number {
     // leetcode MinPriorityQueue is slow for some reason!
     const queue = new Heap<number>((a, b) => a - b);
     for (let i = 0; i < nums.length; i++) {
         if (i < k) {
             queue.enqueue(nums[i]);
-        } else if (nums[i] > queue.front().element) {
+        } else if (nums[i] > queue.front()!) {
             queue.dequeue();
             queue.enqueue(nums[i]);
         }
     }
-    return queue.front().element;
+    return queue.front()!;
 };
 
 function findKthLargest2(nums: number[], k: number): number {

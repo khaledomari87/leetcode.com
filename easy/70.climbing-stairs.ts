b54bf174b3e644f8b16a00fb774b11eeccf64b01
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-30 23:46:18 +0300

    Create 70.climbing-stairs.ts

---
+++ easy/70.climbing-stairs.ts
@@ -0,0 +1,11 @@
+function climbStairs(n: number): number {
+    let output = 1;
+    for (let prev = 1; n > 1; n--) [output, prev] = [prev + output, output];
+    return output;
+}
+
+function climbStairs2(n: number): number {
+    if (n === 1) return n;
+    const goldenRatio = 1.61803398875; // (1 + Math.sqrt(5)) / 2;
+    return Math.round(goldenRatio * (Math.pow(goldenRatio, n) - Math.pow(1 - goldenRatio, n)) / Math.sqrt(5));
+}

1be277b2bb3b99d072fbccf2f844470198c30e4e
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-30 22:10:50 +0300

    Create 509.fibonacci-number.ts

---
+++ medium/509.fibonacci-number.ts
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/fibonacci-number/
+
+function fib(n: number): number {
+    const goldenRatio = 1.618034; // (1 + Math.sqrt(5)) / 2
+    return Math.trunc((Math.pow(goldenRatio, n) - Math.pow(1 - goldenRatio, n)) / Math.sqrt(5));
+    // if (n < 2) return n;
+    // let output = 1;
+    // for (let prev = 0; n > 1; n--) {
+    //     [output, prev] = [prev + output, output];
+    // }
+    // return output;
+}

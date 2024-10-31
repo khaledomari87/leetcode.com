e29b7e983c32487d47df3d15c766bf56012fc55c
Khaled Al-Omari <khaledomari87@gmail.com>
2024-10-31 21:35:47 +0300

    Update 70.climbing-stairs.ts

--- easy/70.climbing-stairs.ts
+++ easy/70.climbing-stairs.ts
@@ -1,3 +1,5 @@
+// https://leetcode.com/problems/climbing-stairs/
+
 function climbStairs(n: number): number {
     let output = 1;
     for (let prev = 1; n > 1; n--) [output, prev] = [prev + output, output];
@@ -5,7 +7,7 @@ function climbStairs(n: number): number {
 }
 
 function climbStairs2(n: number): number {
-    if (n === 1) return n;
+    if (n < 4) return n;
     const goldenRatio = 1.61803398875; // (1 + Math.sqrt(5)) / 2;
     return Math.round(goldenRatio * (Math.pow(goldenRatio, n) - Math.pow(1 - goldenRatio, n)) / Math.sqrt(5));
 }

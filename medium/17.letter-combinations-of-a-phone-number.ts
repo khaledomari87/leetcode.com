1aa1c31b8509062b25e013f2a2840f4585d04533
Khaled Al-Omari <khaledomari87@gmail.com>
2024-11-04 15:11:43 +0300

    Create 17.letter-combinations-of-a-phone-number.ts

---
+++ medium/17.letter-combinations-of-a-phone-number.ts
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/letter-combinations-of-a-phone-number/
+
+function letterCombinations(digits: string): string[] {
+    if (!digits) return [];
+    const map = new Map([
+        ['2', ['a', 'b', 'c']],
+        ['3', ['d', 'e', 'f']],
+        ['4', ['g', 'h', 'i']],
+        ['5', ['j', 'k', 'l']],
+        ['6', ['m', 'n', 'o']],
+        ['7', ['p', 'q', 'r', 's']],
+        ['8', ['t', 'u', 'v']],
+        ['9', ['w', 'x', 'y', 'z']],
+    ]);
+    const output: string[] = [], track: string[] = [], n = digits.length;
+    const rbt = (i: number) => {
+        if (i === n) {
+            output.push(track.join(''));
+            return;
+        }
+        for (const char of map.get(digits[i])!) {
+            track.push(char);
+            rbt(i + 1);
+            track.pop();
+        }
+    };
+    rbt(0);
+    return output;
+}

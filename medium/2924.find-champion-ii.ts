c3fedfc48fbe7e027029ff2c6ea1726efbdafa41
Khaled Al-Omari <khaledomari87@gmail.com>
2024-11-26 15:00:22 +0300

    Create 2924.find-champion-ii.ts

---
+++ medium/2924.find-champion-ii.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/find-champion-ii/
+
+function findChampion(n: number, edges: [number, number][]): number {
+    const nodesWithParents = new Array<boolean>(n);
+    for (const [, dist] of edges) nodesWithParents[dist] = true;
+
+    let output = -1;
+    for (let champ = 0; champ < n; champ++) {
+        if (!nodesWithParents[champ]) {
+            if (output > -1) return -1; // We already have another parentless node
+            output = champ;
+        }
+    }
+    return output;
+}

50d0575abf121ccfc843f47119c2d42d813f2cbe
Khaled Al-Omari <khaledomari87@gmail.com>
2024-12-12 11:24:15 +0300

    Create 2558.take-gifts-from-the-richest-pile.ts

---
+++ easy/2558.take-gifts-from-the-richest-pile.ts
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/take-gifts-from-the-richest-pile/solutions/6138611/max-heap/
+
+import { Heap } from '../assets/heap.ts';
+
+function pickGifts(gifts: number[], k: number): number {
+    const maxHeap = new Heap<number>((a, b) => b - a);
+    for (const g of gifts) {
+        maxHeap.enqueue(g);
+    }
+    for (let i = 0; i < k; i++) {
+        maxHeap.enqueue(Math.floor(Math.sqrt(maxHeap.dequeue()!)));
+    }
+    return maxHeap.toArray().reduce((p, c) => p + c);
+}

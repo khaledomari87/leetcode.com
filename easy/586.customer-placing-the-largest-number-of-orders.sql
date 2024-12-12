329826b9de8af3be216b808d0c0ec125c0444c00
Khaled Al-Omari <khaledomari87@gmail.com>
2024-12-12 20:37:30 +0300

    Create 586.customer-placing-the-largest-number-of-orders.sql

---
+++ easy/586.customer-placing-the-largest-number-of-orders.sql
@@ -0,0 +1,23 @@
+-- https://leetcode.com/problems/customer-placing-the-largest-number-of-orders/solutions/6140355/simplest-solution-group-by-order-by-count-desc/
+
+-- Postgres and MySQL
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC
+LIMIT 1
+
+-- Microsoft SQL
+SELECT TOP 1 customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(*) DESC;
+
+-- Oracle
+SELECT customer_number
+FROM (
+    SELECT customer_number, COUNT(order_number) AS order_count
+    FROM Orders
+    GROUP BY customer_number
+    ORDER BY COUNT(*) DESC
+) WHERE ROWNUM = 1;

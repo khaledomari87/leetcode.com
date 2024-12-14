55c9544a24801813a6f322f154f21cd8866fe878
Khaled Al-Omari <khaledomari87@gmail.com>
2024-12-14 08:48:57 +0300

    Update 1207.unique-number-of-occurrences.ts

--- easy/1207.unique-number-of-occurrences.ts
+++ easy/1207.unique-number-of-occurrences.ts
@@ -1,15 +1,10 @@
 // https://leetcode.com/problems/unique-number-of-occurrences/solutions/6144872/on-solution-using-double-hash-tables-by-y6wy9/
 
 function uniqueOccurrences(arr: number[]): boolean {
-    const map = new Map<number, number>();
-    for (const num of arr) {
-        map.set(num, (map.get(num) || 0) + 1);
-    }
+    const map = arr.reduce((map, num) => map.set(num, (map.get(num) || 0) + 1), new Map<number, number>())
     const set = new Set<number>();
     for (const count of map.values()) {
-        if (set.has(count)) {
-            return false;
-        }
+        if (set.has(count)) return false;
         set.add(count);
     }
     return true;

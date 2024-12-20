b09f71d9aef782588f5e41896f4d4c3a5155d670
Khaled Al-Omari <khaledomari87@gmail.com>
2024-12-20 12:00:50 +0300

    Create 2415.reverse-odd-levels-of-binary-tree.ts

---
+++ medium/2415.reverse-odd-levels-of-binary-tree.ts
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/reverse-odd-levels-of-binary-tree/solutions/6167012/bfs-using-dfs-and-a-hash-table-by-khaled-35cd/
+
+class TreeNode {
+    val: number;
+    left: TreeNode | null;
+    right: TreeNode | null;
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = val === undefined ? 0 : val;
+        this.left = left === undefined ? null : left;
+        this.right = right === undefined ? null : right;
+    }
+}
+
+function reverseOddLevels(root: TreeNode | null): TreeNode | null {
+    const map = new Map<number, TreeNode[]>(); // level => nodes[]
+    const dfs = (node: TreeNode | null, level: number) => {
+        if (!node) return;
+        if (level % 2 === 1) {
+            let arr = map.get(level);
+            if (!arr) {
+                map.set(level, arr = []);
+            }
+            arr.push(node);
+        }
+        dfs(node.left, ++level);
+        dfs(node.right, level);
+    };
+    dfs(root, 0);
+    for (const arr of map.values()) {
+        for (let j = 0, len = arr.length; j < len / 2; j++) {
+            [arr[j].val, arr[len - j - 1].val] = [arr[len - j - 1].val, arr[j].val];
+        }
+    }
+    return root;
+}
+
+// reverseOddLevels(
+//     new TreeNode(
+//         2,
+//         new TreeNode(3, new TreeNode(8), new TreeNode(13)),
+//         new TreeNode(5, new TreeNode(21), new TreeNode(34)),
+//     ),
+// );

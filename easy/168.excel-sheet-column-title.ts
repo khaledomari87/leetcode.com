7fb1f89f433ec167b199dcbcd72fc775b24df8c6
Khaled Al-Omari <khaledomari87@gmail.com>
2024-12-26 11:17:29 +0300

    Update 168.excel-sheet-column-title.ts

--- easy/168.excel-sheet-column-title.ts
+++ easy/168.excel-sheet-column-title.ts
@@ -1,9 +1,10 @@
+// https://leetcode.com/problems/excel-sheet-column-title/solutions/6188284/cannot-be-faster-base-26-log-0ms/
+
 function convertToTitle(num: number): string {
     const A = 'A'.charCodeAt(0), aphabets = 26;
     let output = '';
     while (num-- > 0) {
-        const charCode = (num % aphabets) + A;
-        output = String.fromCharCode(charCode) + output;
+        output = String.fromCharCode((num % aphabets) + A) + output;
         num = Math.floor(num / aphabets);
     }
     return output;

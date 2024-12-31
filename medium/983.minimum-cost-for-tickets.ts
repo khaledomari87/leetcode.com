647a9278c716d3f2fe886e3db1df044deb84052a
Khaled Al-Omari <khaledomari87@gmail.com>
2024-12-31 19:50:41 +0300

    Create 983.minimum-cost-for-tickets.ts

---
+++ medium/983.minimum-cost-for-tickets.ts
@@ -0,0 +1,15 @@
+// https://leetcode.com/problems/minimum-cost-for-tickets/solutions/6210050/bottom-up-tabulation-linear-solution/
+
+function mincostTickets(days: number[], costs: [number, number, number]): number {
+    const cache = new Array<number>(days.length + 1).fill(0);
+    const durToCostMap = [[1, costs[0]], [7, costs[1]], [30, costs[2]]];
+    for (let i = days.length - 1; i >= 0; i--) {
+        cache[i] = Number.POSITIVE_INFINITY;
+        let j = i;
+        durToCostMap.forEach(item => {
+            while (j < days.length && days[j] < days[i] + item[0]) j++;
+            cache[i] = Math.min(cache[i], cache[j] + item[1]);
+        });
+    }
+    return cache[0];
+}

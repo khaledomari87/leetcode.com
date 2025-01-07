858e95080d64a201e03980cca55cdad9bb85b5ee
Khaled Al-Omari <khaledomari87@gmail.com>
2025-01-07 14:22:17 +0300

    Update 1408.string-matching-in-an-array.ts

--- easy/1408.string-matching-in-an-array.ts
+++ easy/1408.string-matching-in-an-array.ts
@@ -1,7 +1,5 @@
 // https://leetcode.com/problems/string-matching-in-an-array/solutions/6243553/brute-force/
 
-import TrieNode from '../assets/TrieNode.ts';
-
 const stringMatching = (words: string[]) => {
     const res: string[] = [];
     for (let i = 0; i < words.length; i++) {

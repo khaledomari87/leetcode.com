d4eb0418d58f5ad8f8a3aa983bf4aea1f8c78b90
Khaled Al-Omari <khaledomari87@gmail.com>
2025-01-12 19:06:39 +0300

    Create 197.rising-temperature.sql

---
+++ easy/197.rising-temperature.sql
@@ -0,0 +1,15 @@
+-- https://leetcode.com/problems/rising-temperature/solutions/6269966/simplest-solution/
+
+-- Oracle / Postgres
+SELECT w.id
+FROM Weather w
+INNER JOIN Weather prev on
+    w.recordDate - prev.recordDate = 1 AND
+    w.temperature > prev.temperature
+
+-- MySQL
+SELECT w.id
+FROM Weather w
+INNER JOIN Weather prev ON
+    DATEDIFF(w.recordDate, prev.recordDate) = 1 AND
+    w.temperature > prev.temperature

a4bd666f4e4ba489f83e3998a030fb953e63a86e
Khaled Al-Omari <khaledomari87@gmail.com>
2025-01-22 21:34:16 +0300

    Update 1765.map-of-highest-peak.ts

--- medium/1765.map-of-highest-peak.ts
+++ medium/1765.map-of-highest-peak.ts
@@ -3,17 +3,22 @@
 function highestPeak(mat: (0 | 1)[][]): number[][] {
     const m = mat.length, n = mat[0].length;
     const res: number[][] = new Array(m);
-    for (let i = 0; i < m; i++) res[i] = new Array(n).fill(-1);
-    const queue: [number, number][] = [];
+    for (let i = 0; i < m; i++) res[i] = new Array(n);
+    let queue: [number, number][] = [];
     mat.forEach((row, i) => row.forEach((bin, j) => bin && queue.push([i, j]) && (res[i][j] = 0)));
-    for (let left = 0; left < queue.length; ++left) {
-        [[-1, 0], [1, 0], [0, -1], [0, 1]].forEach((dir) => {
-            const r = queue[left][0] + dir[0], c = queue[left][1] + dir[1];
-            if (res[r]?.[c] === -1) {
-                res[r][c] = res[queue[left][0]][queue[left][1]] + 1;
-                queue.push([r, c]);
-            }
-        });
+
+    while (queue.length) {
+        const newQ: typeof queue = [];
+        for (const q of queue) {
+            [[-1, 0], [1, 0], [0, -1], [0, 1]].forEach((dir) => {
+                const r = q[0] + dir[0], c = q[1] + dir[1];
+                if (r >= 0 && c >= 0 && r < m && c < n && res[r][c] === undefined) {
+                    res[r][c] = res[q[0]][q[1]] + 1;
+                    newQ.push([r, c]);
+                }
+            });
+        }
+        queue = newQ;
     }
     return res;
 }

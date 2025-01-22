94fda78946c06c105f77cd48c79534133cd5bd40
Khaled Al-Omari <khaledomari87@gmail.com>
2025-01-22 21:20:21 +0300

    Update 1765.map-of-highest-peak.ts

--- medium/1765.map-of-highest-peak.ts
+++ medium/1765.map-of-highest-peak.ts
@@ -1,29 +1,19 @@
 // https://leetcode.com/problems/map-of-highest-peak/solutions/6313706/multi-source-bfs/
 
-function highestPeak(isWater: (0 | 1)[][]): number[][] {
-    const m = isWater.length, n = isWater[0].length;
+function highestPeak(mat: (0 | 1)[][]): number[][] {
+    const m = mat.length, n = mat[0].length;
     const res: number[][] = new Array(m);
-    for (let i = 0; i < m; i++) res[i] = new Array(n);
+    for (let i = 0; i < m; i++) res[i] = new Array(n).fill(-1);
     const queue: [number, number][] = [];
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            if (isWater[i][j]) {
-                queue.push([i, j]);
-                res[i][j] = 0;
-            }
-        }
-    }
-    const dirs = [[-1, 0], [1, 0], [0, -1], [0, 1]];
-    // queue.shift() is O(n), so we use a left pointer
+    mat.forEach((row, i) => row.forEach((bin, j) => bin && queue.push([i, j]) && (res[i][j] = 0)));
     for (let left = 0; left < queue.length; ++left) {
-        const [qr, qc] = queue[left];
-        for (const dir of dirs) {
-            const r = qr + dir[0], c = qc + dir[1];
-            if (r >= 0 && c >= 0 && r < m && c < n && res[r][c] === undefined) {
-                res[r][c] = res[qr][qc] + 1;
+        [[-1, 0], [1, 0], [0, -1], [0, 1]].forEach((dir) => {
+            const r = queue[left][0] + dir[0], c = queue[left][1] + dir[1];
+            if (res[r]?.[c] === -1) {
+                res[r][c] = res[queue[left][0]][queue[left][1]] + 1;
                 queue.push([r, c]);
             }
-        }
+        });
     }
     return res;
 }

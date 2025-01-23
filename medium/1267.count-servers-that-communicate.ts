d42a043487a2b247fcdee7784487537cbe9fe329
Khaled Al-Omari <khaledomari87@gmail.com>
2025-01-23 16:37:47 +0300

    Update 1267.count-servers-that-communicate.ts

--- medium/1267.count-servers-that-communicate.ts
+++ medium/1267.count-servers-that-communicate.ts
@@ -1,16 +1,11 @@
 // https://leetcode.com/problems/count-servers-that-communicate/solutions/6318065/clean-and-efficient/
 
 function countServers(grid: (0 | 1)[][]) {
-    const rc: [number, number][][] = new Array(grid.length); // Rows Counts
-    const cc: [number, number][][] = new Array(grid[0].length); // Columns Counts
-    for (let i = 0; i < grid.length; i++) rc[i] = [];
-    for (let i = 0; i < grid[0].length; i++) cc[i] = [];
-
-    grid.forEach((row, r) => row.forEach((b, c) => b && rc[r].push([r, c]) && cc[c].push([r, c])));
-
+    const rc: number[] = new Array(grid.length).fill(0); // Rows Counts
+    const cc: number[] = new Array(grid[0].length).fill(0); // Columns Counts
+    grid.forEach((row, r) => row.forEach((b, c) => b && ++rc[r] && ++cc[c]));
     return grid.reduce(
-        (res, row, r) =>
-            row.reduce((acc, cell, c) => acc + +(cell && (rc[r].length > 1 || cc[c].length > 1)), res),
+        (res, r, i) => r.reduce((acc, v, j) => acc + +(v && (rc[i] > 1 || cc[j] > 1)), res),
         0,
     );
 }

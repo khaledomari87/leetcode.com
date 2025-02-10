2d933a8342c363ba859839abc844b539a4b34a9d
Khaled Al-Omari <khaledomari87@gmail.com>
2025-02-10 08:05:12 +0300

    Update 3174.clear-digits.ts

--- easy/3174.clear-digits.ts
+++ easy/3174.clear-digits.ts
@@ -1,9 +1,10 @@
 // https://leetcode.com/problems/clear-digits/solutions/6186966/optimum-solution/
 
-function clearDigits(s: string): string {
+function clearDigits(s: string) {
     const output: string[] = [];
     for (let i = s.length - 1, count = 0; i >= 0; i--) {
         isNaN(parseInt(s[i])) ? count > 0 && count-- || output.push(s[i]) : count++;
+        // s[i] >= '0' && s[i] <= '9' ? count++ : count > 0 && count-- || output.push(s[i]);
     }
     // return output.reduce((str, char) => char + str, '');
     return output.reverse().join('');

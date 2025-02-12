a37bfe1717a834df95a44a210f18c70ca5e49da5
Khaled Al-Omari <khaledomari87@gmail.com>
2025-02-12 12:48:45 +0300

    Update 2342.max-sum-of-a-pair-with-equal-sum-of-digits.ts

--- medium/2342.max-sum-of-a-pair-with-equal-sum-of-digits.ts
+++ medium/2342.max-sum-of-a-pair-with-equal-sum-of-digits.ts
@@ -1,17 +1,14 @@
 // https://leetcode.com/problems/max-sum-of-a-pair-with-equal-sum-of-digits/solutions/6411649/optimal-solution/
 
 function maximumSum(nums: number[]) {
-    const groups = new Array<number>(82);
+    const maxes = new Array<number>(82);
     return nums.reduce((res, num) => {
-        let sum = 0, tmp = num;
-        while (tmp > 0) {
-            sum += tmp % 10;
-            tmp = Math.floor(tmp / 10);
-        }
-        if (groups[sum] === undefined) groups[sum] = num;
+        let sum = 0;
+        for (let tmp = num; tmp > 0; tmp = Math.floor(tmp / 10)) sum += tmp % 10;
+        if (maxes[sum] === undefined) maxes[sum] = num;
         else {
-            res = Math.max(res, groups[sum] + num);
-            groups[sum] = Math.max(groups[sum], num);
+            res = Math.max(res, maxes[sum] + num);
+            maxes[sum] = Math.max(maxes[sum], num);
         }
         return res;
     }, -1);

f3688283c42d2def4f00abac08e8de14c91b8001
Khaled Al-Omari <khaledomari87@gmail.com>
2025-02-20 14:24:05 +0300

    Update 1980.find-unique-binary-string.ts

--- medium/1980.find-unique-binary-string.ts
+++ medium/1980.find-unique-binary-string.ts
@@ -2,3 +2,10 @@
 
 const findDifferentBinaryString = (nums: string[]) =>
     nums.reduce((res, bin, idx) => `${res}${+!+bin[idx]}`, '');
+
+const findDifferentBinaryStringRandom = (nums: string[]): string => {
+    const set = new Set(nums.map((bin) => Number.parseInt(bin, 2)));
+    let rand = 0;
+    while (set.has(rand)) rand = Math.floor(Math.random() * (2 ** nums.length));
+    return rand.toString(2).padStart(nums.length, '0');
+};

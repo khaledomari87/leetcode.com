5551f5080d415b826269a2eabb008a62483ed46f
Khaled Al-Omari <khaledomari87@gmail.com>
2025-02-21 07:25:49 +0300

    Create 1261.find-elements-in-a-contaminated-binary-tree.ts

---
+++ medium/1261.find-elements-in-a-contaminated-binary-tree.ts
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/find-elements-in-a-contaminated-binary-tree/solutions/6449287/hash-set-and-depth-first-search/
+
+import type TreeNode from '../assets/TreeNode.ts';
+
+class FindElements {
+    private set = new Set<number>();
+    constructor(root: TreeNode | null) {
+        const dfs = (node: TreeNode | null, val: number) => {
+            if (!node) return;
+            this.set.add(val);
+            dfs(node.left, val * 2 + 1);
+            dfs(node.right, val * 2 + 2);
+        };
+        dfs(root, 0);
+    }
+    find = (target: number) => this.set.has(target);
+}

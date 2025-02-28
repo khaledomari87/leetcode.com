a15bd39ce92d1f0230ec75e37022cee07f898c07
Khaled Al-Omari <khaledomari87@gmail.com>
2025-02-28 16:24:26 +0300

    Create 804.unique-morse-code-words.ts

---
+++ easy/804.unique-morse-code-words.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/unique-morse-code-words/solutions/6477652/hash-set/
+
+function uniqueMorseRepresentations(words: string[]): number {
+    const morseCodes = [
+        '.-',   '-...', '-.-.', '-..',  '.',    '..-.', '--.',
+        '....', '..',   '.---', '-.-',  '.-..', '--',   '-.',
+        '---',  '.--.', '--.-', '.-.',  '...',  '-',    '..-',
+        '...-', '.--',  '-..-', '-.--', '--..',
+    ];
+    const set = new Set<string>(), a = 'a'.charCodeAt(0);
+    for (const word of words) {
+        const code = new Array<string>(word.length);
+        for (let i = 0; i < word.length; i++) {
+            code[i] = morseCodes[word.charCodeAt(i) - a];
+        }
+        set.add(code.join(''));
+    }
+    return set.size;
+}

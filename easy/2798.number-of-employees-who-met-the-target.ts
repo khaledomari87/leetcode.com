44ce49809a1e49f04a9ee58d1b7ef573917ac0e7
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-01 22:46:40 +0300

    Update 2798.number-of-employees-who-met-the-target.ts

--- easy/2798.number-of-employees-who-met-the-target.ts
+++ easy/2798.number-of-employees-who-met-the-target.ts
@@ -1,4 +1,4 @@
 // https://leetcode.com/problems/number-of-employees-who-met-the-target/
 
-const numberOfEmployeesWhoMetTarget = (hrs: number[], t: number): number =>
-    hrs.reduce((prev, curr) => prev + (curr >= t ? 1 : 0), 0);
+const numberOfEmployeesWhoMetTarget = (hrs: number[], t: number) =>
+    hrs.reduce((sum, val) => sum + +(val >= t), 0);

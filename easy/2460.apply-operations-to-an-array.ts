9f23d26fbcf8f7da2432613254764f1dd21692e3
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-01 10:45:26 +0300

    Create 2460.apply-operations-to-an-array.ts

---
+++ easy/2460.apply-operations-to-an-array.ts
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/apply-operations-to-an-array/solutions/6480289/two-approaches/
+
+function applyOperationsFilter(nums: number[]) {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] <<= 1;
+            nums[i + 1] = 0;
+        }
+    }
+    const res = nums.filter((x) => x !== 0);
+    return res.concat(new Array<number>(nums.length - res.length).fill(0));
+}
+
+function applyOperations(nums: number[]): number[] {
+    for (let i = 0; i < nums.length - 1; i++) {
+        if (nums[i] === nums[i + 1]) {
+            nums[i] <<= 1;
+            nums[i + 1] = 0;
+        }
+    }
+    for (let l = 0, r = 0; r < nums.length; r++) {
+        if (nums[r] !== 0) [nums[l++], nums[r]] = [nums[r], nums[l]];
+    }
+    return nums;
+}

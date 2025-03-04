96c1bf8326adf139b297e0091ef0120064a77678
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-04 13:42:02 +0300

    Update 1780.check-if-number-is-a-sum-of-powers-of-three.ts

--- medium/1780.check-if-number-is-a-sum-of-powers-of-three.ts
+++ medium/1780.check-if-number-is-a-sum-of-powers-of-three.ts
@@ -1,4 +1,4 @@
-// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/solutions/6494459/ternary-representation/
+// https://leetcode.com/problems/check-if-number-is-a-sum-of-powers-of-three/solutions/6494459/optimal-solutions-two-approaches/
 
 function checkPowersOfThree(n: number) {
     while (n > 0) {
@@ -6,4 +6,16 @@ function checkPowersOfThree(n: number) {
         n = Math.floor(n / 3);
     }
     return true;
+}
+
+function checkPowersOfThree2(n: number) {
+    let res = 0, power = 14; // 3^14 = max(n)
+    while (power >= 0) {
+        const candidate = 3 ** power--;
+        if (res + candidate <= n) {
+            res += candidate;
+            if (res === n) return true;
+        }
+    }
+    return false;
 }

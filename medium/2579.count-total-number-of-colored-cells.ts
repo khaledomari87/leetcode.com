2073a271c39580e9c2892453a1ff929a5813460c
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-05 09:29:48 +0300

    Update 2579.count-total-number-of-colored-cells.ts

--- medium/2579.count-total-number-of-colored-cells.ts
+++ medium/2579.count-total-number-of-colored-cells.ts
@@ -1,9 +1,9 @@
 // https://leetcode.com/problems/count-total-number-of-colored-cells/solutions/6498473/iterative-and-formula-solutions/
 
 function coloredCells(n: number) {
-    let res = 1, addition = 0;
-    while (--n) res += addition += 4;
+    let res = 1;
+    while (--n) res += 4 * n;
     return res;
 }
 
-const coloredCellsFormula = (n: number) => 1 + n * (n - 1) * 2;
+const coloredCellsFormula = (n: number) => 1 + 2 * n * (n - 1);

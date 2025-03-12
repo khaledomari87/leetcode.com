4753886c062bd7192f741a0f2c60876c457b1b72
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-12 10:01:41 +0300

    Create 2529.maximum-count-of-positive-integer-and-negative-integer.ts

---
+++ easy/2529.maximum-count-of-positive-integer-and-negative-integer.ts
@@ -0,0 +1,13 @@
+function maximumCount(nums: number[]) {
+    let low = nums.length, high = low, l = 0, r = nums.length - 1;
+    while (l <= r) {
+        const mid = l + r >>> 1;
+        nums[mid] < 0 ? l = mid + 1 : r = (low = mid) - 1;
+    }
+    l = 0, r = nums.length - 1;
+    while (l <= r) {
+        const mid = l + r >>> 1;
+        nums[mid] > 0 ? r = (high = mid) - 1 : l = mid + 1;
+    }
+    return Math.max(low, nums.length - high);
+}

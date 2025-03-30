2499d21fa79a5a29b6dc7255fcb634456c6517a1
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-30 08:56:27 +0300

    Update 763.partition-labels.ts

--- medium/763.partition-labels.ts
+++ medium/763.partition-labels.ts
@@ -1,17 +1,14 @@
-// https://leetcode.com/problems/partition-labels/solutions/6594813/greedy/
+// https://leetcode.com/problems/partition-labels/solutions/6594813/clean-greedy-solution/
 
 function partitionLabels(s: string) {
     const res: number[] = [],
         a = 'a'.charCodeAt(0),
-        code = (i: number) => s.charCodeAt(i) - a,
         lasts: number[] = new Array(26);
-    for (let i = 0; i < s.length; i++) lasts[code(i)] = i;
+    for (let i = 0; i < s.length; i++) lasts[s.charCodeAt(i) - a] = i;
 
     for (let i = 0, L = 0, R = 0; i < s.length; i++) {
-        R = Math.max(R, lasts[code(i)]);
-        if (i === R) {
-            res.push(R - L + 1);
-            L = R + 1;
+        if (i === (R = Math.max(R, lasts[s.charCodeAt(i) - a]))) {
+            res.push(R - L + 1), L = R + 1;
         }
     }
     return res;

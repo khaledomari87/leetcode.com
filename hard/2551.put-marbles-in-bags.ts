3218706fc58e1e34b53bd7998c420e1f0b4f47d3
Khaled Al-Omari <khaledomari87@gmail.com>
2025-03-31 07:09:01 +0300

    Update 2551.put-marbles-in-bags.ts

--- hard/2551.put-marbles-in-bags.ts
+++ hard/2551.put-marbles-in-bags.ts
@@ -1,5 +1,7 @@
 // https://leetcode.com/problems/put-marbles-in-bags/solutions/6598266/sort-and-greedy/
 
+import { Heap } from '../assets/Heap.ts';
+
 function putMarbles(weights: number[], k: number) {
     const paris = new Array<number>(weights.length - 1);
     for (let i = 0; i < weights.length - 1; i++) {
@@ -11,4 +13,20 @@ function putMarbles(weights: number[], k: number) {
         res += paris.at(-i - 1)! - paris[i];
     }
     return res;
+}
+
+function putMarblesHeap(weights: number[], k: number): number {
+    const pairSums = new Array<number>(weights.length - 1);
+    for (let i = 0; i < weights.length - 1; i++) {
+        pairSums[i] = weights[i] + weights[i + 1];
+    }
+    const minHeap = new Heap((a, b) => a - b, [...pairSums]);
+    const maxHeap = new Heap((a, b) => b - a, pairSums);
+    [minHeap, maxHeap].forEach((heap) => {
+        while (heap.size > k - 1) heap.dequeue();
+    });
+    return (
+        minHeap.toArray().reduce((a, b) => a + b, 0) -
+        maxHeap.toArray().reduce((a, b) => a + b, 0)
+    );
 }

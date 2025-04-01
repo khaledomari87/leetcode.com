e93ee698a664d063d4b500c04d25b6b7e6335fe8
Khaled Al-Omari <khaledomari87@gmail.com>
2025-04-01 07:29:21 +0300

    Create 2140.solving-questions-with-brainpower.ts

---
+++ medium/2140.solving-questions-with-brainpower.ts
@@ -0,0 +1,29 @@
+// https://leetcode.com/problems/solving-questions-with-brainpower/solutions/6601999/two-approaches/
+
+function mostPoints(questions: [number, number][]) {
+    const cache = new Array<number>(questions.length);
+    const dfs = (i: number) => {
+        if (i >= questions.length) return 0;
+        if (!cache[i]) {
+            cache[i] = Math.max(
+                questions[i][0] + dfs(i + questions[i][1] + 1),
+                dfs(i + 1),
+            );
+        }
+        return cache[i];
+    };
+    return dfs(0);
+}
+
+function mostPointsDP(questions: [number, number][]) {
+    const dp = new Array<number>(questions.length).fill(0);
+    dp[questions.length - 1] = questions.at(-1)![0];
+    for (let i = questions.length - 2; i >= 0; i--) {
+        const skipped = questions[i][1] + i + 1;
+        if (skipped < questions.length) {
+            questions[i][0] += dp[skipped];
+        }
+        dp[i] = Math.max(questions[i][0], dp[i + 1]);
+    }
+    return dp[0];
+}

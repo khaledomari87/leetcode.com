fcb9133a070b1dc77ed7c9ba1570250d7bc01cf7
Khaled Al-Omari <khaledomari87@gmail.com>
2025-04-27 04:06:51 +0300

    Update 3392.count-subarrays-of-length-three-with-a-condition.ts

--- easy/3392.count-subarrays-of-length-three-with-a-condition.ts
+++ easy/3392.count-subarrays-of-length-three-with-a-condition.ts
@@ -1,9 +1,12 @@
 // https://leetcode.com/problems/count-subarrays-of-length-three-with-a-condition/solutions/6188244/optimum-solution/
 
-const countSubarrays = (nums: number[]): number => {
-    let output = 0;
+const countSubarrays = (nums: number[]) => {
+    let res = 0;
     for (let i = 1; i < nums.length - 1; i++) {
-        nums[i - 1] + nums[i + 1] === nums[i] / 2 && output++;
+        res += +(nums[i - 1] + nums[i + 1] === nums[i] / 2);
     }
-    return output;
+    return res;
 };
+
+const countSubarrays2 = (nums: number[]) =>
+    nums.slice(1, -1).reduce((res, num, i) => res + +(nums[i] + nums[i + 2] === num / 2), 0);

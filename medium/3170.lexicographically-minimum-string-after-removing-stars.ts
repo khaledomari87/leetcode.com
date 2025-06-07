d4b18ef984b2d8dc01443fc937ac948f0650c662
Khaled Al-Omari <khaledomari87@gmail.com>
2025-06-07 16:34:48 +0300

    Update 3170.lexicographically-minimum-string-after-removing-stars.ts

--- medium/3170.lexicographically-minimum-string-after-removing-stars.ts
+++ medium/3170.lexicographically-minimum-string-after-removing-stars.ts
@@ -1,11 +1,11 @@
 // https://leetcode.com/problems/lexicographically-minimum-string-after-removing-stars/solutions/6819824/greedy-clean-ts-js/
 
 function clearStars(s: string) {
-    const counts: number[][] = Array.from({ length: 26 }, () => []);
+    const indices: number[][] = Array.from({ length: 26 }, () => []);
     const res = s.split(''), a = 'a'.charCodeAt(0);
     const index = (i: number) => s.charCodeAt(i) - a;
     res.forEach((char, i) =>
-        char !== '*' ? counts[index(i)].push(i) : res[counts.find((arr) => arr.length)!.pop()!] = '*'
+        char !== '*' ? indices[index(i)].push(i) : res[indices.find((arr) => arr.length)!.pop()!] = '*'
     );
     return res.filter((c) => c !== '*').join('');
 }

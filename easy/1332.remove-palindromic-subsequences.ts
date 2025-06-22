00a61a8a8bfde8f0d938eb76f00501868632a4c3
Khaled Al-Omari <khaledomari87@gmail.com>
2025-06-22 07:13:54 +0300

    Create 1332.remove-palindromic-subsequences.ts

---
+++ easy/1332.remove-palindromic-subsequences.ts
@@ -0,0 +1,4 @@
+// https://leetcode.com/problems/remove-palindromic-subsequences/solutions/6870848/one-line/
+
+const removePalindromeSub = (s: string) =>
+    2 - +(s === s.split('').reverse().join(''));

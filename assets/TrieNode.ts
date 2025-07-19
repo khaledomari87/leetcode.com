f931634f976cbafc242eb93c6a4b8e45eb42b1fe
Khaled Al-Omari <khaledomari87@gmail.com>
2025-07-19 10:14:47 +0300

    Update TrieNode.ts

--- assets/TrieNode.ts
+++ assets/TrieNode.ts
@@ -1,30 +1,19 @@
+
 export default class TrieNode {
     private _isWord = false;
-    private _characters;
-    constructor() {
-        this._characters = new Map<string, TrieNode>();
-    }
+    private _characters: Map<string, TrieNode> = new Map<string, TrieNode>();
+
     set isWord(bool: boolean) {
         this._isWord = bool;
     }
-
     get isWord() {
         return this._isWord;
     }
-
     get characters() {
         return this._characters;
     }
 
-    hasCharAt(s: string) {
-        return this.characters.has(s);
-    }
-
-    getCharAt(s: string): TrieNode | undefined {
-        return this.characters.get(s);
-    }
-
-    setCharAt(s: string) {
-        return this.characters.set(s, new TrieNode());
-    }
+    hasCharAt = (s: string) => this.characters.has(s);
+    getCharAt = (s: string) => this._characters.get(s);
+    setCharAt = (s: string) => this._characters.set(s, new TrieNode()).get(s)!;
 }

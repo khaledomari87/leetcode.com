aa652f7b1c21aa958ec7403c475177f3cfc9027e
Khaled Al-Omari <khaledomari87@gmail.com>
2025-07-27 23:01:53 +0300

    Create 1517.find-users-with-valid-e-mails.sql

---
+++ easy/1517.find-users-with-valid-e-mails.sql
@@ -0,0 +1,16 @@
+-- https://leetcode.com/problems/find-users-with-valid-e-mails/solutions/7013238/universal-query/
+
+-- Postgres
+SELECT *
+FROM Users
+WHERE mail ~ '^[a-zA-Z][a-zA-Z0-9\.\-_]*@leetcode\.com$';
+
+-- MySQL
+SELECT *
+FROM Users
+WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\\.com$', 'c');
+
+-- Oracle
+SELECT *
+FROM Users
+WHERE REGEXP_LIKE(mail, '^[a-zA-Z][a-zA-Z0-9._-]*@leetcode\.com$');

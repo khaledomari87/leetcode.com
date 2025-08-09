00c1b13cb377782b68bf8b8700b1a0f557e5b036
Khaled Al-Omari <khaledomari87@gmail.com>
2025-08-09 12:53:55 +0300

    Update 231.power-of-two.ts

--- easy/231.power-of-two.ts
+++ easy/231.power-of-two.ts
@@ -1,3 +1,11 @@
-// https://leetcode.com/problems/power-of-two/solutions/6273557/one-liner-without-loops/
+// https://leetcode.com/problems/power-of-two/solutions/6273557/one-liner-constant-time/
 
 const isPowerOfTwo = (n: number) => Math.log2(n) % 1 === 0;
+
+function isPowerOfTwoTraversal(n: number) {
+    if (n < 1) return false;
+    while (n > 1) {
+        if ((n /= 2) % 1 !== 0) return false;
+    }
+    return true;
+}

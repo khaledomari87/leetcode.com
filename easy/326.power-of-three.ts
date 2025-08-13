3d7bf9fd19a717e2c2e20c442352e0e556e26cce
Khaled Al-Omari <khaledomari87@gmail.com>
2025-08-13 09:32:28 +0300

    Update 326.power-of-three.ts

--- easy/326.power-of-three.ts
+++ easy/326.power-of-three.ts
@@ -1,3 +1,11 @@
 // https://leetcode.com/problems/power-of-three/solutions/6275060/constant-time-and-space-one-liner/
 
 const isPowerOfThree = (n: number) => (Math.log10(n) / Math.log10(3)) % 1 === 0;
+
+function isPowerOfThreeIterator(n: number) {
+    if (n < 1) return false;
+    while (n > 1) {
+        if ((n /= 3) % 1 !== 0) return false;
+    }
+    return true;
+}

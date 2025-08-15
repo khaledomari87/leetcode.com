0c8cde73818116f71b13888fa6d4c1e16ceda755
Khaled Al-Omari <khaledomari87@gmail.com>
2025-08-15 22:33:17 +0300

    Update 342.power-of-four.ts

--- easy/342.power-of-four.ts
+++ easy/342.power-of-four.ts
@@ -1,3 +1,11 @@
 // https://leetcode.com/problems/power-of-four/solutions/6279356/one-liner-constant-time-and-space/
 
 const isPowerOfFour = (n: number) => Math.log2(n) / 2 % 1 === 0;
+
+function isPowerOfFourIterator(n: number) {
+    if (n < 1) return false;
+    while (n > 1) {
+        if ((n /= 4) % 1 !== 0) return false;
+    }
+    return true;
+}

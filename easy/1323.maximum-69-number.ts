8b1dff31f4adb085c40cc2d15f8a71d7daf6628e
Khaled Al-Omari <khaledomari87@gmail.com>
2025-08-16 11:00:17 +0300

    Update 1323.maximum-69-number.ts

--- easy/1323.maximum-69-number.ts
+++ easy/1323.maximum-69-number.ts
@@ -1,3 +1,12 @@
 // https://leetcode.com/problems/maximum-69-number/solutions/6851510/one-line/
 
 const maximum69Number = (n: number) => +String(n).replace('6', '9');
+
+function maximum69NumberTraversal(n: number) {
+    for (let i = 0, s = String(n); i < s.length; i++) {
+        if (s[i] === '6') {
+            return +`${s.slice(0, i)}9${s.slice(i + 1)}`;
+        }
+    }
+    return n;
+}

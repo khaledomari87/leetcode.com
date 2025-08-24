718622623043f8906a006ea8c0e64977328db794
Khaled Al-Omari <khaledomari87@gmail.com>
2025-08-24 22:54:48 +0300

    Create 1662.check-if-two-string-arrays-are-equivalent.ts

---
+++ easy/1662.check-if-two-string-arrays-are-equivalent.ts
@@ -0,0 +1,3 @@
+// https://leetcode.com/problems/check-if-two-string-arrays-are-equivalent/solutions/7118519/one-line/
+
+const arrayStringsAreEqual = (a: string[], b: string[]) => a.join('') === b.join('');

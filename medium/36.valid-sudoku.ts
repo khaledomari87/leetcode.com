0e7adb81ce442af3a178de429ab87929ddc76d9d
Khaled Al-Omari <khaledomari87@gmail.com>
2025-08-30 10:27:19 +0300

    Update 36.valid-sudoku.ts

--- medium/36.valid-sudoku.ts
+++ medium/36.valid-sudoku.ts
@@ -1,49 +1,28 @@
-// https://leetcode.com/problems/valid-sudoku/solutions/5904076/looping-over-the-matrix-once-only-check-the-sub-boxes-formulas/
+// https://leetcode.com/problems/valid-sudoku/solutions/5904076/math-formulas/
 
-const isValidSudokuSequence = (arr: string[]) => {
+const isValid = (arr: string[]) => {
     const seen = new Set<string>();
     for (const char of arr) {
         if (char !== '.') {
-            if (seen.has(char)) {
-                return false;
-            }
+            if (seen.has(char)) return false;
             seen.add(char);
         }
     }
     return true;
 };
 
-function isValidSudoku(board: string[][]): boolean {
-    const length = 9;
-    const boxLength = 3;
-    const cols = new Array<Array<string>>(length);
-    const boxes = new Array<Array<string>>(length);
+function isValidSudoku(board: string[][]) {
+    const length = 9, boxLength = 3, floor = Math.floor;
+    const cols: string[][] = Array.from({ length }, () => new Array(length));
+    const boxes: string[][] = Array.from({ length }, () => new Array(length));
     for (let i = 0; i < length; i++) {
-        if (!isValidSudokuSequence(board[i])) {
-            return false;
-        }
+        if (!isValid(board[i])) return false;
         for (let j = 0; j < length; j++) {
-            if (!cols[j]) {
-                cols[j] = new Array<string>(length);
-            }
             cols[j][i] = board[i][j];
-
-            const boxIndex = boxLength * Math.trunc(i / boxLength) +
-                Math.trunc(j / boxLength);
+            const boxIndex = boxLength * floor(i / boxLength) + floor(j / boxLength);
             const eleIndex = boxLength * (i % boxLength) + j % boxLength;
-            if (!boxes[boxIndex]) {
-                boxes[boxIndex] = new Array<string>(length);
-            }
             boxes[boxIndex][eleIndex] = board[i][j];
         }
     }
-    // return !cols.some(v => !isValidSudokuSequence(v)) && !boxes.some(v => !isValidSudokuSequence(v));
-    for (let i = 0; i < length; i++) {
-        if (
-            !isValidSudokuSequence(cols[i]) || !isValidSudokuSequence(boxes[i])
-        ) {
-            return false;
-        }
-    }
-    return true;
+    return !cols.some((v) => !isValid(v)) && !boxes.some((v) => !isValid(v));
 }

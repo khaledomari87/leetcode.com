41fa54e3f610fcc76bdbf6248a2a7af8e9d78501
Khaled Al-Omari <khaledomari87@gmail.com>
2025-09-02 07:00:35 +0300

    Create 3025.find-the-number-of-ways-to-place-people-i.ts

---
+++ medium/3025.find-the-number-of-ways-to-place-people-i.ts
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/solutions/7146542/sort-enumeration/
+
+function numberOfPairs(points: [number, number][]) {
+    points.sort((a, b) => a[0] - b[0] || b[1] - a[1]);
+    let res = 0;
+    for (let i = 0, n = points.length - 1; i < n; i++) {
+        let xLow = points[i][0] - 1, yLow = -Infinity;
+        const xHigh = Infinity, yHigh = points[i][1] + 1;
+        for (let j = i + 1; j < points.length; j++) {
+            if (
+                points[j][0] > xLow && points[j][0] < xHigh &&
+                points[j][1] > yLow && points[j][1] < yHigh
+            ) {
+                res++, xLow = points[j][0], yLow = points[j][1];
+            }
+        }
+    }
+    return res;
+}

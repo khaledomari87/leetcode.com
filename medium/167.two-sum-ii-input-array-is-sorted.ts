a56c88851a7eea6260dec7406f741d8c7509a8a5
Khaled Al-Omari <khaledomari87@gmail.com>
2025-10-16 14:53:01 +0300

    Update 167.two-sum-ii-input-array-is-sorted.ts

--- medium/167.two-sum-ii-input-array-is-sorted.ts
+++ medium/167.two-sum-ii-input-array-is-sorted.ts
@@ -1,14 +1,10 @@
-// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solutions/5907362/simple-efficient-and-readable/
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/solutions/7279677/two-pointers/
 
-function twoSum(numbers: number[], target: number): number[] {
-    let left = 0,
-        right = numbers.length - 1;
-    while (left < right) {
-        const sum = numbers[left] + numbers[right];
-        if (sum === target) {
-            return [left + 1, right + 1];
-        }
-        sum > target ? right-- : left++;
+function twoSum(nums: number[], target: number) {
+    for (let l = 0, r = nums.length - 1; l < r;) {
+        const sum = nums[l] + nums[r];
+        if (sum === target) return [l + 1, r + 1];
+        sum > target ? r-- : l++;
     }
-    return [];
+    throw new Error('Invalid Input!');
 }

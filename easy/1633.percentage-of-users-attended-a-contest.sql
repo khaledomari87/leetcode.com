5f1d917b3a60d0ace4650239534a7daad9cfc5a3
Khaled Al-Omari <khaledomari87@gmail.com>
2025-10-18 10:19:22 +0300

    Update 1633.percentage-of-users-attended-a-contest.sql

--- easy/1633.percentage-of-users-attended-a-contest.sql
+++ easy/1633.percentage-of-users-attended-a-contest.sql
@@ -1,12 +1,10 @@
---
+-- https://leetcode.com/problems/percentage-of-users-attended-a-contest/solutions/7282704/universal-query-for-all-servers/
 
 -- MySQL / PostgreSQL / Oracle / MSSQL
-WITH global_count AS (
-    SELECT COUNT(user_id) as counts FROM Users
-)
+WITH total AS (SELECT COUNT(user_id) as counts FROM Users)
 SELECT contest_id,
-    ROUND(COUNT(user_id) * 100.0 / AVG(g.counts), 2) AS percentage
-FROM Register r
-CROSS JOIN global_count g
-GROUP BY contest_id
-ORDER BY percentage DESC, contest_id
+    ROUND(COUNT(user_id) * 100.0 / counts, 2) AS percentage
+FROM Register
+CROSS JOIN total
+GROUP BY contest_id, counts
+ORDER BY percentage DESC, contest_id;

251cc7d15e1108a03efea55511ef314420dd08f4
Khaled Al-Omari <khaledomari87@gmail.com>
2025-10-25 08:18:17 +0300

    Update 1716.calculate-money-in-leetcode-bank.ts

--- easy/1716.calculate-money-in-leetcode-bank.ts
+++ easy/1716.calculate-money-in-leetcode-bank.ts
@@ -8,3 +8,16 @@ function totalMoney(n: number) {
     }
     return res;
 }
+
+// deno-fmt-ignore
+function totalMoney2(n: number) {
+    const ArithmeticSum = (m: number) => m * (m + 1) / 2;
+    const fullWeeks = Math.floor(n / 7);
+    const lastWeekRemainder = n % 7;
+    return (
+        ArithmeticSum(7) * fullWeeks +     // Money for all full weeks
+        ArithmeticSum(fullWeeks - 1) * 7 + // Money for the +1 on each Monday
+        ArithmeticSum(lastWeekRemainder) + // Remainder money
+        fullWeeks * lastWeekRemainder      // Remainder money for the +1
+    );
+}

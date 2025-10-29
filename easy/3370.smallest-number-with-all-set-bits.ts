8b5e53df92f5d18cdf028141d41d7e63eacf60bc
Khaled Al-Omari <khaledomari87@gmail.com>
2025-10-29 15:20:09 +0300

    Update 3370.smallest-number-with-all-set-bits.ts

--- easy/3370.smallest-number-with-all-set-bits.ts
+++ easy/3370.smallest-number-with-all-set-bits.ts
@@ -1,3 +1,4 @@
 // https://leetcode.com/problems/smallest-number-with-all-set-bits/solutions/7310251/one-line/
 
 const smallestNumber = (n: number) => parseInt('1'.repeat(n.toString(2).length), 2);
+const smallestNumberLog = (n: number) => (1 << Math.floor(Math.log2(n)) + 1) - 1;

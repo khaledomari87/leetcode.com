445dc501c3b17b6aa24ac29986ccb94af4a8e421
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-02 11:20:57 +0300

    Update 2257.count-unguarded-cells-in-the-grid.ts

--- medium/2257.count-unguarded-cells-in-the-grid.ts
+++ medium/2257.count-unguarded-cells-in-the-grid.ts
@@ -1,6 +1,6 @@
 // https://leetcode.com/problems/count-unguarded-cells-in-the-grid/solutions/6068683/readable-and-efficient-o-n-m/
 
-function countUnguarded(m: number, n: number, guards: [number, number][], walls: typeof guards): number {
+function countUnguarded(m: number, n: number, guards: [number, number][], walls: typeof guards) {
     const grid = new Array<number[]>(m);
     for (let i = 0; i < grid.length; i++) grid[i] = new Array(n);
 
@@ -13,44 +13,25 @@ function countUnguarded(m: number, n: number, guards: [number, number][], walls:
     for (const c of walls) grid[c[0]][c[1]] = W;
 
     let guardedCount = 0;
-    for (const cell of guards) {
-        const row = cell[0];
-        const col = cell[1];
-        // South
-        for (
-            let r = row + 1;
-            r < m && grid[r][col] !== (grid[r][col] & Sc) && grid[r][col] !== G && grid[r][col] !== W;
-            r++
-        ) {
-            !grid[r][col] && guardedCount++;
-            grid[r][col] |= Sc;
+    for (const [row, col] of guards) {
+        let r = row + 1; // South
+        while (r < m && grid[r][col] !== (grid[r][col] & Sc) && grid[r][col] !== G && grid[r][col] !== W) {
+            guardedCount += +!grid[r][col], grid[r++][col] |= Sc;
         }
-        // North
-        for (
-            let r = row - 1;
-            r > -1 && grid[r][col] !== (grid[r][col] & Sc) && grid[r][col] !== G && grid[r][col] !== W;
-            r--
-        ) {
-            !grid[r][col] && guardedCount++;
-            grid[r][col] |= Sc;
+
+        r = row - 1; // North
+        while (r > -1 && grid[r][col] !== (grid[r][col] & Sc) && grid[r][col] !== G && grid[r][col] !== W) {
+            guardedCount += +!grid[r][col], grid[r--][col] |= Sc;
         }
-        // East
-        for (
-            let c = col + 1;
-            c < n && grid[row][c] !== (grid[row][c] & Sr) && grid[row][c] !== G && grid[row][c] !== W;
-            c++
-        ) {
-            !grid[row][c] && guardedCount++;
-            grid[row][c] |= Sr;
+
+        let c = col + 1; // East
+        while (c < n && grid[row][c] !== (grid[row][c] & Sr) && grid[row][c] !== G && grid[row][c] !== W) {
+            guardedCount += +!grid[row][c], grid[row][c++] |= Sr;
         }
-        // West
-        for (
-            let c = col - 1;
-            c > -1 && grid[row][c] !== (grid[row][c] & Sr) && grid[row][c] !== G && grid[row][c] !== W;
-            c--
-        ) {
-            !grid[row][c] && guardedCount++;
-            grid[row][c] |= Sr;
+
+        c = col - 1; // West
+        while (c > -1 && grid[row][c] !== (grid[row][c] & Sr) && grid[row][c] !== G && grid[row][c] !== W) {
+            guardedCount += +!grid[row][c], grid[row][c--] |= Sr;
         }
     }
     return m * n - guardedCount - guards.length - walls.length;

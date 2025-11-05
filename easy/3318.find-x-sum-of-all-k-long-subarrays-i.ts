1d38e3417c0bbf877f6b5813ab6e36ca57ee9d00
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-05 11:12:08 +0300

    Update 3318.find-x-sum-of-all-k-long-subarrays-i.ts

--- easy/3318.find-x-sum-of-all-k-long-subarrays-i.ts
+++ easy/3318.find-x-sum-of-all-k-long-subarrays-i.ts
@@ -8,16 +8,14 @@ function findXSum(nums: number[], k: number, x: number) {
     for (let right = k - 1; right < n; right++) {
         counts.set(nums[right], (counts.get(nums[right]) || 0) + 1);
         const left = right - k + 1;
-        res[left] = [...counts]
-            .sort((a, b) => b[1] - a[1] || b[0] - a[0])
-            .slice(0, x)
-            .reduce(
-                (s, item) => s + item[0] * item[1],
-                0,
-            );
+        let subArr = [...counts];
+        if (counts.size > x) {
+            subArr = subArr.sort((a, b) => b[1] - a[1] || b[0] - a[0]).slice(0, x);
+        }
+        res[left] = subArr.reduce((s, item) => s + item[0] * item[1], 0);
         const first = counts.get(nums[left])!;
-        if (!first) counts.delete(nums[left]);
-        else counts.set(nums[left], first - 1);
+        if (first) counts.set(nums[left], first - 1);
+        else counts.delete(nums[left]);
     }
     return res;
 }

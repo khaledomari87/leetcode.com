f9362ac45c5a1080bc55fe7af71aff6a6168857d
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-17 12:06:45 +0300

    Update 1437.check-if-all-1s-are-at-least-length-k-places-away.ts

--- easy/1437.check-if-all-1s-are-at-least-length-k-places-away.ts
+++ easy/1437.check-if-all-1s-are-at-least-length-k-places-away.ts
@@ -1,7 +1,9 @@
 // https://leetcode.com/problems/check-if-all-1s-are-at-least-length-k-places-away/solutions/6949184/simple-and-optimal/
 
 function kLengthApart(nums: (0 | 1)[], k: number) {
-    for (let L = nums.indexOf(1), R = L + 1; R < nums.length; R++) {
+    let L = nums.indexOf(1);
+    if (L < 0) return true;
+    for (let R = L + 1, n = nums.length; R < n; R++) {
         if (nums[R]) {
             if (R - L - 1 < k) return false;
             L = R;

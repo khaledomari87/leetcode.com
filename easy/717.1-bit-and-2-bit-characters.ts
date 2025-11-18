a9746b9510f2dbe1188db9c6b49afb53c66c8361
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-18 07:44:49 +0300

    Update 717.1-bit-and-2-bit-characters.ts

--- easy/717.1-bit-and-2-bit-characters.ts
+++ easy/717.1-bit-and-2-bit-characters.ts
@@ -1,7 +1,8 @@
 // https://leetcode.com/problems/1-bit-and-2-bit-characters/solutions/6434751/increment-pointer/
 
 function isOneBitCharacter(bits: number[]) {
+    const n = bits.length - 1;
     let i = 0;
-    for (const len = bits.length - 1; i < len; i += bits[i] + 1);
-    return i === bits.length - 1;
+    while (i < n) i += bits[i] + 1;
+    return i === n;
 }

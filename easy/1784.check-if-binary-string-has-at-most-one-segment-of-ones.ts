728d7ae68b940b523f340a2879a12df3bb911f91
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-21 14:57:30 +0300

    Create 1784.check-if-binary-string-has-at-most-one-segment-of-ones.ts

---
+++ easy/1784.check-if-binary-string-has-at-most-one-segment-of-ones.ts
@@ -0,0 +1,3 @@
+// https://leetcode.com/problems/check-if-binary-string-has-at-most-one-segment-of-ones/solutions/7364628/one-line-01-not-in-s/
+
+const checkOnesSegment = (s: string) => !s.includes('01');

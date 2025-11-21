16aa88678a25cc3290289a70e6fc642b0458bdd2
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-21 12:23:21 +0300

    Update 1930.unique-length-3-palindromic-subsequences.ts

--- medium/1930.unique-length-3-palindromic-subsequences.ts
+++ medium/1930.unique-length-3-palindromic-subsequences.ts
@@ -1,16 +1,13 @@
-// https://leetcode.com/problems/unique-length-3-palindromic-subsequences/solutions/6227984/optimum-solution-10ms-short-and-readable/
+// https://leetcode.com/problems/unique-length-3-palindromic-subsequences/solutions/6227984/linear-solution/
 
+// deno-fmt-ignore
 function countPalindromicSubsequence(s: string) {
-    let output = 0;
-    for (let code = 'a'.charCodeAt(0), end = code + 26; code < end; code++) {
-        const char = String.fromCharCode(code);
-        let left = s.indexOf(char);
-        const right = s.lastIndexOf(char);
+    let res = 0;
+    for (let c = 'a'; c <= 'z'; c = String.fromCharCode(c.charCodeAt(0) + 1)) {
         const seen = new Set<string>();
-        while (seen.size < 26 && ++left < right) {
-            seen.add(s[left]);
-        }
-        output += seen.size;
+        for (let left = s.indexOf(c), right = s.lastIndexOf(c);
+            seen.size < 26 && ++left < right;) seen.add(s[left]);
+        res += seen.size;
     }
-    return output;
+    return res;
 }

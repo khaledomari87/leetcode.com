6b84fcdd7c7f53b956e2d6c1fc24e78b02ab984f
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-22 11:00:34 +0300

    Update 3190.find-minimum-operations-to-make-all-elements-divisible-by-three.ts

--- easy/3190.find-minimum-operations-to-make-all-elements-divisible-by-three.ts
+++ easy/3190.find-minimum-operations-to-make-all-elements-divisible-by-three.ts
@@ -1,3 +1,3 @@
 // https://leetcode.com/problems/find-minimum-operations-to-make-all-elements-divisible-by-three/solutions/6186977/one-line-optimum-solution/
 
-const minimumOperations = (n: number[]): number => n.reduce((s, i) => s + +(i % 3 !== 0), 0);
+const minimumOperations = (n: number[]) => n.reduce((s, v) => s + +(v % 3 > 0), 0);

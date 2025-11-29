d74cca6708a0f19df2bb3b0a76affcd0fb8e2f3b
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-29 14:40:46 +0300

    Create 2215.find-the-difference-of-two-arrays.ts

---
+++ easy/2215.find-the-difference-of-two-arrays.ts
@@ -0,0 +1,4 @@
+// https://leetcode.com/problems/find-the-difference-of-two-arrays/solutions/7381211/one-line-linear-om-n/
+
+const findDifference = (a: number[], b: number[]) =>
+    [a, b].map((a) => new Set(a)).map((s, i, a) => [...s].filter((v) => !a[+!i].has(v)));

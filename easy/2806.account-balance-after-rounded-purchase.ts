7c03d1b49cbf95f679ab210e6c936524bd62e5eb
Khaled Al-Omari <khaledomari87@gmail.com>
2025-11-29 23:18:47 +0300

    Create 2806.account-balance-after-rounded-purchase.ts

---
+++ easy/2806.account-balance-after-rounded-purchase.ts
@@ -0,0 +1,3 @@
+// https://leetcode.com/problems/account-balance-after-rounded-purchase/solutions/7381892/one-line/
+
+const accountBalanceAfterPurchase = (purchaseAmount: number) => 100 - 10 * Math.round(purchaseAmount / 10);

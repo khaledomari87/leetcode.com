be9b1ffff43931c60081631523637a8f5de59e29
Khaled Al-Omari <khaledomari87@gmail.com>
2025-12-11 20:00:33 +0300

    Create 3741.minimum-distance-between-three-equal-elements-ii.ts

---
+++ medium/3741.minimum-distance-between-three-equal-elements-ii.ts
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/minimum-distance-between-three-equal-elements-ii/solutions/7407339/sliding-window-linear-solution/
+
+function minimumDistance(nums: number[]) {
+    const indices = new Map<number, number[]>();
+    for (let i = 0; i < nums.length; i++) {
+        if (!indices.has(nums[i])) indices.set(nums[i], []);
+        indices.get(nums[i])!.push(i);
+    }
+    let res = Infinity;
+    for (const a of indices.values()) {
+        for (let i = 2; i < a.length; i++) {
+            res = Math.min(res, 2 * a[i] - 2 * a[i - 2]);
+        }
+    }
+    return isFinite(res) ? res : -1;
+}

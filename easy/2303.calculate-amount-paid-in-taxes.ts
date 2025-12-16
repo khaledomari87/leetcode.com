c279e748b43e965a3772ab13a3f3a55a85544435
Khaled Al-Omari <khaledomari87@gmail.com>
2025-12-16 18:35:04 +0300

    Add kill current debug task

--- easy/2303.calculate-amount-paid-in-taxes.ts
+++ easy/2303.calculate-amount-paid-in-taxes.ts
@@ -1,4 +1,5 @@
 // https://leetcode.com/problems/calculate-amount-paid-in-taxes/solutions/7417552/linear-solution/
+
 function calculateTax(brackets: [number, number][], income: number) {
     let res = 0, prev = 0;
     for (const [upper, precent] of brackets) {

5f5eab9cb187a57f31ad35ab40e13b3d2b9c7393
Khaled Al-Omari <khaledomari87@gmail.com>
2025-12-16 14:13:30 +0300

    Create 2303.calculate-amount-paid-in-taxes.ts

---
+++ easy/2303.calculate-amount-paid-in-taxes.ts
@@ -0,0 +1,10 @@
+// https://leetcode.com/problems/calculate-amount-paid-in-taxes/solutions/7417552/linear-solution/
+function calculateTax(brackets: [number, number][], income: number) {
+    let res = 0, prev = 0;
+    for (const [upper, precent] of brackets) {
+        const curr = Math.min(income, upper) - prev;
+        res += curr * precent / 100;
+        if ((prev += curr) >= income) break;
+    }
+    return res;
+}

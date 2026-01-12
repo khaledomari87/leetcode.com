74296fcf8c8ccb03466666432aa12c22155e5f2c
Khaled Al-Omari <khaledomari87@gmail.com>
2026-01-12 08:05:31 +0300

    Update 1266.minimum-time-visiting-all-points.ts

--- easy/1266.minimum-time-visiting-all-points.ts
+++ easy/1266.minimum-time-visiting-all-points.ts
@@ -1,11 +1,6 @@
 // https://leetcode.com/problems/minimum-time-visiting-all-points/solutions/6829289/clean-ts-js/
 
-const minTimeToVisitAllPoints = (points: number[][]) =>
-    points.reduce(
-        (res, p, i) =>
-            res + (!i ? 0 : Math.max(
-                Math.abs(p[0] - points[i - 1][0]),
-                Math.abs(p[1] - points[i - 1][1]),
-            )),
-        0,
-    );
+// deno-fmt-ignore-file
+const { abs, max } = Math;
+const minTimeToVisitAllPoints = (points: number[][]) => points.reduce((r, [x, y], i) =>
+    r + (!i-- ? 0 : max(abs(x - points[i][0]), abs(y - points[i][1]))), 0);

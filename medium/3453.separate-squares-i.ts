426341da8764dbdcc099d4b30a6be6ee0d7c8fa1
Khaled Al-Omari <khaledomari87@gmail.com>
2026-01-13 14:58:50 +0300

    Update 3453.separate-squares-i.ts

--- medium/3453.separate-squares-i.ts
+++ medium/3453.separate-squares-i.ts
@@ -1,15 +1,15 @@
 // https://leetcode.com/problems/separate-squares-i/solutions/7491300/binary-search/
 
-function separateSquares(squares: number[][]) {
-    const err = 1 / 1e5, { min, max } = Math;
+function separateSquares(squares: number[][], margin = 1 / 1e5) {
+    const { min, max } = Math;
     let bot = 0, top = 0, totalArea = 0;
     for (const [, y, l] of squares) totalArea += l * l, top = max(top, y + l);
-    for (const halfArea = totalArea /= 2; top - bot > err;) {
+    for (const halfArea = totalArea /= 2; top - bot > margin;) {
         const m = (top + bot) / 2;
         const botArea = squares.reduce((a, [, y, l]) => a + max(0, min(m - y, l)) * l, 0);
         botArea >= halfArea ? top = m : bot = m;
         // const topArea = squares.reduce((a, [, y, l]) => a + max(0, min(l + y - m, l)) * l, 0);
         // topArea <= halfArea ? top = m : bot = m;
     }
-    return top;
+    return top; // or return bot, even though top != bot
 }

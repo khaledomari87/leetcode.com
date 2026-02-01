a7e12d4dc44599f7b52454f108b3dcab21e51a88
Khaled Al-Omari <khaledomari87@gmail.com>
2026-02-01 11:07:21 +0300

    Update 3010.divide-an-array-into-subarrays-with-minimum-cost-i.ts

--- easy/3010.divide-an-array-into-subarrays-with-minimum-cost-i.ts
+++ easy/3010.divide-an-array-into-subarrays-with-minimum-cost-i.ts
@@ -1,11 +1,10 @@
 // https://leetcode.com/problems/divide-an-array-into-subarrays-with-minimum-cost-i/solutions/7144443/optimal-solution/
 
 function minimumCost(nums: number[]) {
-    const first = nums[0], min = Math.min;
     let min1 = Infinity, min2 = min1;
-    for (let i = 1, n = nums.length; i < n; i++) {
+    for (let min = Math.min, i = 1, n = nums.length; i < n; i++) {
         if (nums[i] < min2) min1 = min2, min2 = nums[i];
         else if (nums[i] < min1) min1 = nums[i];
     }
-    return first + min1 + min2;
+    return nums[0] + min1 + min2;
 }

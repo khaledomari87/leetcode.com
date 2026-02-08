09c86f7730f25c22ea359f95ad4a527066a83afa
Khaled Al-Omari <khaledomari87@gmail.com>
2026-02-08 15:55:41 +0300

    Update 110.balanced-binary-tree.ts

--- easy/110.balanced-binary-tree.ts
+++ easy/110.balanced-binary-tree.ts
@@ -1,7 +1,6 @@
 import type TreeNode from '../assets/TreeNode.ts';
 
-const h = (node: TreeNode | null): number =>
-    node ? 1 + Math.max(h(node.left), h(node.right)) : 0;
+const h = (node: TreeNode | null): number => node ? 1 + Math.max(h(node.left), h(node.right)) : 0;
 
 const isBalanced = (root: TreeNode | null): boolean =>
     !root ? true : Math.abs(h(root.left) - h(root.right)) < 2 &&

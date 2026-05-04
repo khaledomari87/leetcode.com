6dc08a400138240dce7a3b7437c577c51217d52c
Khaled Al-Omari <khaledomari87@gmail.com>
2026-05-04 10:57:52 +0300

    Update 48.rotate-image.ts

--- medium/48.rotate-image.ts
+++ medium/48.rotate-image.ts
@@ -1,21 +1,15 @@
-// https://leetcode.com/problems/rotate-image/solutions/5896502/transpose-then-horizontal-reflection/
+// https://leetcode.com/problems/rotate-image/solutions/8136460/transpose-then-horizontal-reflection/
 
-/**
- Do not return anything, modify matrix in-place instead.
- */
-function rotate(matrix: number[][]): void {
+function rotate(matrix: number[][]) {
     const n = matrix.length;
     for (let i = 0; i < n; i++) {
-        for (let j = 0; j < i; j++) { // OR (let j = i + 1; j < n; j++)
+        for (let j = 0; j < i; j++) { // for (let j = i + 1; j < n; j++) {
             [matrix[i][j], matrix[j][i]] = [matrix[j][i], matrix[i][j]];
         }
     }
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < n / 2; j++) {
-            [matrix[i][j], matrix[i][n - j - 1]] = [
-                matrix[i][n - j - 1],
-                matrix[i][j],
-            ];
+            [matrix[i][j], matrix[i][n - j - 1]] = [matrix[i][n - j - 1], matrix[i][j]];
         }
     }
 }
